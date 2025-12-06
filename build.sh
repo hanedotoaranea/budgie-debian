17102e96a34d52e608b3a4672bc10bec95db867e
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2025-12-06 20:28:58 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
 lb config -d testing \
-          --distribution "trixie" \
-          --binary-images iso-hybrid \
-          --archive-areas "main contrib non-free non-free-firmware" \
-          --debootstrap-options "--variant=minbase" \
-          --mirror-bootstrap "http://deb.debian.org/debian/" \
-          --mirror-chroot "http://deb.debian.org/debian/" \
-          --bootappend-live "boot=live components username=user hostname=trixie-budgie"
+    --distribution "trixie" \
+    --binary-images iso-hybrid \
+    --archive-areas "main contrib non-free non-free-firmware" \
+    --debootstrap-options "--variant=minbase" \
+    --mirror-bootstrap "http://deb.debian.org/debian/" \
+    --mirror-chroot "http://deb.debian.org/debian/" \
+    --bootappend-live "boot=live components username=user hostname=debian-budgie" \
+    --clean \
+    --apt-indices false \
+    --apt-recommends false \
+    --security false \
+    --updates false \
+    --backports false \
+    --packages-lists "minimal" \
+    --firmware-binary \
+    --firmware-chroot
 sudo lb build
